7f1bde07214d92f6758ef7077e395fa6dd7b0e82
Muhammad Talha <63364406+hmtalha786@users.noreply.github.com>
2022-08-02 23:59:20 +0500

    Store data to MongoDB via POST

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const User = require('../models/User');
+const bodyParser = require("body-parser");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    obj = {
-        a: 'thios',
-        number: 34
-    }
-    res.json(obj);
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(express.json());
+
+// Create a User using: POST "/api/auth/". Doesn't require Auth
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const user = User(req.body);
+    user.save();
+    res.send(req.body);
 })
 
 module.exports = router;
