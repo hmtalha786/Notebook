e6a3a13271b24fd35394ba493a4b346cdcefa314
Muhammad Talha <63364406+hmtalha786@users.noreply.github.com>
2022-08-14 22:04:59 +0500

    Some Code Clean up for further work

--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -24,11 +24,10 @@ const Navbar = () => {
                         <li className="nav-item">
                             <Link className={`nav-link ${location.pathname === "/about" ? "active" : ""}`} to="/about">About</Link>
                         </li>
-
                     </ul>
                     <form className="d-flex">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
+                        <input className="form-control me-3" type="search" placeholder="Search Your Notes" aria-label="Search" />
+                        <button className="btn btn-outline-primary" type="submit">Search</button>
                     </form>
                 </div>
             </div>
--- Frontend/src/context/notes/NoteState.js
+++ Frontend/src/context/notes/NoteState.js
@@ -1,24 +1,9 @@
 import NoteContext from "./noteContext";
-import { useState } from "react";
 
 const NoteState = (props) => {
-    const s1 = {
-        "name": "steve",
-        "class": "12"
-    }
-    const [state, setState] = useState(s1);
-
-    const update = () => {
-        setTimeout(() => {
-            setState({
-                "name": "Bob",
-                "class": "10"
-            })
-        }, 1000);
-    }
-
+    
     return (
-        <NoteContext.Provider value={{ state: state, update: update }}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider>
     )
